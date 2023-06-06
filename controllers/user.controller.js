ae9730b53317ba920ea0adaa47e23cc8284a6c22
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-06-07 02:08:06 +0600

    user login service created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,6 @@
-const { createAuserService } = require("../services/user.service");
+const { createAuserService, loginAuserService } = require("../services/user.service");
 
-// save a user -------------------------------
+// save a user controller-------------------------------
 exports.createAuser = async(req, res, next)=>{
     try {
         const data = req.body;
@@ -15,7 +15,46 @@ exports.createAuser = async(req, res, next)=>{
     } catch (error) {
         res.status(400).json({
             status: 'error',
-            massage: "Data inserted Error",
+            massage: "User inserted Error",
+            error: error.message
+        })
+    }
+}
+
+// login a user controller --------------------------------
+exports.loginAuser = async(req, res, next)=>{
+    try {
+        const {email,password}= req.body                     //take data from body
+
+ 
+        if(!password || !email){                              //cheak user email and password exists
+            res.status(200).json({
+                status: 'failed',
+                massage: "Please insert your email and password",
+            })
+        }
+
+
+        const user = await loginAuserService(email)        // if user exists send db the email
+
+
+        if(!user){                                          // if not user send response
+            res.status(200).json({
+                status: 'failed',
+                massage: "user doesn't exists",
+             
+       })
+        }
+
+        res.status(200).json({
+            status: 'success',
+            massage: "User logged in Successfull!",
+            data: result
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'error',
+            massage: "User logged in Error",
             error: error.message
         })
     }
--- services/user.service.js
+++ services/user.service.js
@@ -6,3 +6,10 @@ exports.createAuserService = async(data)=>{
     const result = await User.create(data)
     return result;
 }
+
+
+// login a user service--------------------------------
+exports.loginAuserService = async(email)=>{
+    const result = await User.findOne({email:email})
+    return result;
+}
